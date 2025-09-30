83a269a31b6c4bbda6b166f9250546d60b753016
Rishitha Reddy <rishithareddychalla@gmail.com>
2025-09-30 23:46:26 +0530

    initial_sos

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,59 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "com.beep.sos"
+    compileSdk = 36
+    ndkVersion = "27.0.12077973"
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+
+    defaultConfig {
+        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        applicationId = "com.beep.sos"
+        // You can update the following values to match your application needs.
+        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        minSdk = flutter.minSdkVersion
+        targetSdk = 35
+        versionCode = 7
+        versionName = "7.0"
+    }
+    
+
+     signingConfigs {
+        create("release") {
+            storeFile = file(project.property("MYAPP_UPLOAD_STORE_FILE") as String)
+            storePassword = project.property("MYAPP_UPLOAD_STORE_PASSWORD") as String
+            keyAlias = project.property("MYAPP_UPLOAD_KEY_ALIAS") as String
+            keyPassword = project.property("MYAPP_UPLOAD_KEY_PASSWORD") as String
+        }
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            isShrinkResources = false
+            signingConfig = signingConfigs.getByName("release")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+}
+
+flutter {
+    source = "../.."
+}
---
+++ android/app/src/main/kotlin/com/example/sos_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.beep.sos
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
