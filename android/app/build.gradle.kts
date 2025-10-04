7e4019d34a33fe5685dcc3c48e13bef186c9b089
Rishitha Reddy <rishithareddychalla@gmail.com>
2025-10-04 20:53:40 +0530

    final

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -17,7 +17,7 @@ plugins {
 }
 
 android {
-    namespace = "com.beep.sos"
+    namespace = "com.sgitosuhrd.user"
     compileSdk = 36
     ndkVersion = "27.0.12077973"
 
@@ -35,13 +35,13 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.beep.sos"
+        applicationId = "com.sgitosuhrd.user"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = 35
-        versionCode = 7
-        versionName = "7.0"
+        versionCode = 9
+        versionName = "9.0"
     }
     
 
--- android/app/src/main/kotlin/com/example/sos_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/sos_app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.beep.sos
+package com.sgitosuhrd.user
 
 import io.flutter.embedding.android.FlutterActivity
 
